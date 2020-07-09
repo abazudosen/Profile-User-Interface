678152ce84e8b264e77cf0952a740b0995c10802
Abasiono Udosen <abasionoudosen@Abaz.local>
2020-07-09 15:38:31 +0300

    done for now

--- App.js
+++ App.js
@@ -1,21 +1,301 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  SafeAreaView,
+  ScrollView,
+} from "react-native";
+import { Ionicons, MaterialIcons } from "@expo/vector-icons";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={styles.titleBar}>
+          <Ionicons name="ios-arrow-back" size={24} color="#52575d"></Ionicons>
+          <Ionicons name="md-more" size={24} color="#52575d"></Ionicons>
+        </View>
+
+        <View style={{ alignSelf: "center" }}>
+          <View style={styles.profileImage}>
+            <Image
+              source={require("./assets/profile-pic.jpg")}
+              style={styles.image}
+              resizeMode="center"
+            ></Image>
+          </View>
+          <View style={styles.dm}>
+            <MaterialIcons
+              name="chat"
+              size={18}
+              color="#dfd8c8"
+            ></MaterialIcons>
+          </View>
+          <View style={styles.active}></View>
+          <View style={styles.add}>
+            <Ionicons
+              name="ios-add"
+              size={48}
+              color="#dfd8c8"
+              style={{ marginTop: 6, marginLeft: 2 }}
+            ></Ionicons>
+          </View>
+        </View>
+
+        <View style={styles.infoContainer}>
+          <Text style={[styles.text, { fontWeight: "200", fontSize: 36 }]}>
+            Renisha Jnr
+          </Text>
+          <Text style={[styles.text, { color: "#aeb5bc", fontSize: 14 }]}>
+            Designer
+          </Text>
+        </View>
+
+        <View style={styles.statsContainer}>
+          <View style={styles.statsBox}>
+            <Text style={[styles.text, { fontSize: 24 }]}>211</Text>
+            <Text style={[styles.text, styles.subText]}>Posts</Text>
+          </View>
+          <View
+            style={[
+              styles.statsBox,
+              {
+                borderColor: "#dfd8c8",
+                borderLeftWidth: 1,
+                borderRightWidth: 1,
+              },
+            ]}
+          >
+            <Text style={[styles.text, { fontSize: 24 }]}>112,023</Text>
+            <Text style={[styles.text, styles.subText]}>Followers</Text>
+          </View>
+          <View style={styles.statsBox}>
+            <Text style={[styles.text, { fontSize: 24 }]}>101</Text>
+            <Text style={[styles.text, styles.subText]}>Following</Text>
+          </View>
+        </View>
+
+        <View style={{ marginTop: 32 }}>
+          <ScrollView horizontal={true} showHorizontalScrollIndicator={false}>
+            <View style={styles.mediaImageContainer}>
+              <Image
+                source={require("./assets/media1.jpg")}
+                style={styles.image}
+                resizeMode="cover"
+              ></Image>
+            </View>
+            <View style={styles.mediaImageContainer}>
+              <Image
+                source={require("./assets/media2.jpg")}
+                style={styles.image}
+                resizeMode="cover"
+              ></Image>
+            </View>
+            <View style={styles.mediaImageContainer}>
+              <Image
+                source={require("./assets/media3.jpg")}
+                style={styles.image}
+                resizeMode="cover"
+              ></Image>
+            </View>
+            <View style={styles.mediaImageContainer}>
+              <Image
+                source={require("./assets/media4.jpg")}
+                style={styles.image}
+                resizeMode="cover"
+              ></Image>
+            </View>
+            <View style={styles.mediaImageContainer}>
+              <Image
+                source={require("./assets/media6.jpg")}
+                style={styles.image}
+                resizeMode="cover"
+              ></Image>
+            </View>
+            <View style={styles.mediaImageContainer}>
+              <Image
+                source={require("./assets/media8.jpg")}
+                style={styles.image}
+                resizeMode="cover"
+              ></Image>
+            </View>
+          </ScrollView>
+          <View style={styles.mediaCount}>
+            <Text
+              style={[
+                styles.text,
+                { fontSize: 24, color: "#dfd8c8", fontWeight: "300" },
+              ]}
+            >
+              70
+            </Text>
+            <Text
+              style={[
+                styles.text,
+                { fontSize: 12, color: "#dfd8c8", textTransform: "uppercase" },
+              ]}
+            >
+              Media
+            </Text>
+          </View>
+        </View>
+
+        <Text style={[styles.subText, styles.recent]}>Recent Activity</Text>
+
+        <View style={{ alignItems: "center" }}>
+          <View style={styles.recentItem}>
+            <View style={styles.recentItemIndicator}></View>
+            <View style={{ width: 250 }}>
+              <Text
+                style={[styles.text, { color: "#41444b", fontWeight: "300" }]}
+              >
+                Started following{" "}
+                <Text style={{ fontWeight: "400" }}>
+                  James Okon and{" "}
+                  <Text style={{ fontWeight: "400" }}>AbazUdosen</Text>
+                </Text>
+              </Text>
+            </View>
+          </View>
+
+          <View style={styles.recentItem}>
+            <View style={styles.recentItemIndicator}></View>
+            <View style={{ width: 250 }}>
+              <Text
+                style={[styles.text, { color: "#41444b", fontWeight: "300" }]}
+              >
+                Started following{" "}
+                <Text style={{ fontWeight: "400" }}>UDE Anderson</Text>
+              </Text>
+            </View>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+  },
+  text: {
+    fontFamily: "HelveticaNeue",
+    color: "#52575d",
+  },
+  subText: {
+    fontSize: 12,
+    color: "#aeb5bc",
+    textTransform: "uppercase",
+    fontWeight: "500",
+  },
+  image: {
+    flex: 1,
+    width: undefined,
+    height: undefined,
+  },
+  titleBar: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: 24,
+    marginHorizontal: 16,
+  },
+  profileImage: {
+    width: 200,
+    height: 200,
+    borderRadius: 100,
+    overflow: "hidden",
+  },
+  dm: {
+    backgroundColor: "#41444b",
+    position: "absolute",
+    top: 20,
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  active: {
+    backgroundColor: "#34ffb9",
+    position: "absolute",
+    bottom: 28,
+    left: 10,
+    padding: 4,
+    height: 20,
+    width: 20,
+    borderRadius: 10,
+  },
+  add: {
+    backgroundColor: "#41444b",
+    position: "absolute",
+    bottom: 0,
+    right: 0,
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  infoContainer: {
+    alignSelf: "center",
+    alignItems: "center",
+    marginTop: 16,
+  },
+  statsContainer: {
+    flexDirection: "row",
+    alignSelf: "center",
+    marginTop: 32,
+  },
+  statsBox: {
+    alignItems: "center",
+    flex: 1,
+  },
+  mediaImageContainer: {
+    width: 180,
+    height: 200,
+    borderRadius: 12,
+    overflow: "hidden",
+    marginHorizontal: 10,
+  },
+  mediaCount: {
+    backgroundColor: "#41444b",
+    position: "absolute",
+    top: "50%",
+    marginTop: -50,
+    marginLeft: 30,
+    width: 100,
+    height: 100,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 12,
+    shadowColor: "rgba(0, 0, 0, 0.38)",
+    shadowOffset: { width: 0, height: 10 },
+    shadowRadius: 20,
+    shadowOpacity: 1,
+  },
+  recent: {
+    marginLeft: 78,
+    marginTop: 32,
+    marginBottom: 6,
+    fontSize: 10,
+  },
+  recentItem: {
+    flexDirection: "row",
+    alignItems: "flex-start",
+    marginBottom: 16,
+  },
+  recentItemIndicator: {
+    backgroundColor: "#CADFAB",
+    padding: 4,
+    height: 12,
+    width: 12,
+    borderRadius: 6,
+    marginTop: 3,
+    marginRight: 20,
   },
 });
